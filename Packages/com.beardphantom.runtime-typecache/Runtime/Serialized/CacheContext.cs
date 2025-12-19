64823cf68a765106e01079bcd46a888949b5a9bf
Mika Notarnicola <mika@beardphantom.com>
2025-12-18 22:59:32 -0800

    Add mechanism to avoid visiting types multiple times

---
+++ Packages/com.beardphantom.runtime-typecache/Runtime/Serialized/CacheContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BeardPhantom.RuntimeTypeCache.Serialized
+{
+    internal class CacheContext
+    {
+        public readonly ITypeCacheSource TypeCacheSource;
+
+        private readonly HashSet<Type> _visitedTypes = new();
+
+        public CacheContext(ITypeCacheSource typeCacheSource)
+        {
+            TypeCacheSource = typeCacheSource;
+        }
+
+        public bool TryVisit(Type type)
+        {
+            return _visitedTypes.Add(type);
+        }
+    }
+}
